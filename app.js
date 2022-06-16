11ae618e7e399d18213f415d9565be6a21f526f6
Miraya Abrodi <mirayaabrodi@gmail.com>
2022-06-16 12:17:25 +0200

    🚀 fetch APi ( get img every tie we click on the btn)

--- app.js
+++ app.js
@@ -1,8 +1,27 @@
-const foxImage = document.querySelector('#cat-result')
-const dogImage = document.querySelector('#dog-result')
-const foxBtn = document.querySelector('#cat-btn')
-const dogBtn = document.querySelector('#dog-btn')
+const foxImage = document.querySelector("#fox-result");
+const dogImage = document.querySelector("#dog-result");
+const foxBtn = document.querySelector("#fox-btn");
+const dogBtn = document.querySelector("#dog-btn");
 
+foxBtn.addEventListener("click", getRandomFox);
+dogBtn.addEventListener("click", getRandomDog);
 
-foxBtn.addEventListener('click', getRandomFox)
-dogBtn.addEventListener('click', getRandomDog)
+function getRandomFox() {
+  fetch("https://randomfox.ca/floof/")
+    .then((res) => res.json())
+    .then((data) => {
+      foxImage.innerHTML = `<img src="${data.image}"/>`;
+    });
+}
+
+function getRandomDog() {
+  fetch("https://random.dog/woof.json")
+    .then((res) => res.json())
+    .then((data) => {
+      if (data.url.includes(".mp4")) {
+        getRandomDog();
+      } else {
+        dogImage.innerHTML = `<img src="${data.url}"/>`;
+      }
+    });
+}
