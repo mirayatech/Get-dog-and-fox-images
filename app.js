75e2787377b87dbe36762ee9c0002109b4b82bc7
Miraya Abrodi <mirayaabrodi@gmail.com>
2022-06-18 21:00:28 +0200

    add effect to btn

--- app.js
+++ app.js
@@ -7,21 +7,28 @@ foxBtn.addEventListener("click", getRandomFox);
 dogBtn.addEventListener("click", getRandomDog);
 
 function getRandomFox() {
+  foxBtn.innerText = 'Loading...'
   fetch("https://randomfox.ca/floof/")
     .then((res) => res.json())
     .then((data) => {
       foxImage.innerHTML = `<img src="${data.image}"/>`;
+      foxBtn.innerText = 'Get Fox'
+
     });
 }
 
 function getRandomDog() {
+  dogBtn.innerText = 'Loading...'
   fetch("https://random.dog/woof.json")
     .then((res) => res.json())
     .then((data) => {
       if (data.url.includes(".mp4")) {
         getRandomDog();
+        dogBtn.innerText = 'Get Dog'
+
       } else {
         dogImage.innerHTML = `<img src="${data.url}"/>`;
+        dogBtn.innerText = 'Get Dog'
       }
     });
 }
